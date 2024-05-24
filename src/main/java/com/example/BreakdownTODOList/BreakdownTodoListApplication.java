14f227c6108feb28f9cdeae4d5f506e3e240da9e
unknown <syedgul.cs17@iba-suk.edu.pk>
2024-05-24 21:41:23 +0500

    Project initialzied

---
+++ src/main/java/com/example/BreakdownTODOList/BreakdownTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.BreakdownTODOList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakdownTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakdownTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BreakdownTODOList/BreakdownTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BreakdownTODOList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakdownTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
